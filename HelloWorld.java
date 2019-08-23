a2d07cbf61d5d3c0f5f3f83fe3399387ccccc64d
ptanguyet <ptanguyet069@gmail.com>
2019-08-23 10:15:55 +0700

    Second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -22,6 +22,7 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
 		System.out.println("This is automated build");
+		System.out.println("This is automated build again");
     }
 
 }
